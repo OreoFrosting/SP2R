d40f582ecdafcdc92945d5bd5e14ef3dd8427366
OreoFrosting <alexsun@haverford.org>
2018-05-22 12:25:18 -0400

    Testing for adding my personal website

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from .models import Post
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+ def alex_site(request):
+ 	return render(request, 'blog/Alexhome.html', {})
 # Create your views here.
