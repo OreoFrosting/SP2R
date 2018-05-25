8b18349598707b1184268bfd6d723a30ba8c2aec
AlexSun <alexsun@haverford.org>
2018-05-25 08:34:34 -0400

    linked my site to the landing page

--- chindict/urls.py
+++ chindict/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.landing_page, name='landing_page'),
     url(r'$', views.alexsite, name='alexsite'),
+    url(r'$', views.alexscope, name='alexscope'),
 ]
--- chindict/views.py
+++ chindict/views.py
@@ -3,4 +3,6 @@ def landing_page(request):
     return render(request, 'chindict/landing_page.html', {})
 def alexsite(request):
 	return render(request, 'chindict/alexsite.html', {})
+def alexscope(request):
+	return render(request, 'chindict/alexscope.html', {})
 # Create your views here.
