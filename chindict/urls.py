9ed444283f8d6ab146bfc296dc118771f1042145
AlexSun <alexsun@haverford.org>
2018-05-25 09:04:03 -0400

    able to link websites now!

--- chindict/urls.py
+++ chindict/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.landing_page, name='landing_page'),
-    url(r'$', views.alexsite, name='alexsite'),
-    url(r'$', views.alexscope, name='alexscope'),
-    url(r'$', views.brycesite, name='brycesite'),
+    url(r'alexsite/$', views.alexsite, name='alexsite'),
+    url(r'alexscope/$', views.alexscope, name='alexscope'),
+    url(r'brycesite/$', views.brycesite, name='brycesite'),
 
 ]
