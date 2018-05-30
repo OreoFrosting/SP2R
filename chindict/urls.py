de38074ba85ff4cc239eac9fc1dd57f77ad57431
AlexSun <alexsun@haverford.org>
2018-05-30 11:47:47 -0400

    made changes to url and views for medhanie's site

--- chindict/urls.py
+++ chindict/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'alexsite/$', views.alexsite, name='alexsite'),
     url(r'alexscope/$', views.alexscope, name='alexscope'),
     url(r'brycesite/$', views.brycesite, name='brycesite'),
-    url(r'nick_index/$', views.nick_index, name='nick_index')
-    url(r'Medhanie/fortnite_home/$', views.Medhanie/fortnite_home, name='fortnite_home')
+    url(r'nick_index/$', views.nick_index, name='nick_index'),
+    url(r'Medhanie/fortnite_home/$', views.fortnite_home, name='fortnite_home')
 
 ]
